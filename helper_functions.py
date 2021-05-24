b94879311b82c1ef74e9d276c16648cc56024c9c
Mikael Roto <mikael.roto@aastat.fi>
2021-05-24 11:05:21 +0300

    Added regex support to find text between last word of header and first of footer

--- helper_functions.py
+++ helper_functions.py
@@ -50,11 +50,15 @@ def remove_empty_lines(block_of_text):
 
 def get_chapter_from_pdf_txt(pdf_text):
     txt = repr(pdf_text)
-    found = re.findall(pattern=f"[\d]{4}[A-z0-9\\ \n-:()]*{settings['RTF_conversion_first_word_in_footer']}", string=txt)[0]
+    start_word = settings["RTF_conversion_last_word_in_header"]
+    end_word = settings["RTF_conversion_first_word_in_footer"]
+    try:
+        found = re.search(pattern=f"(?<={start_word})([A-z0-9\\ \n-:()])*(?={end_word})", string=txt).group(0)
+    except:
+        found = re.search(pattern=f"([A-z0-9\\ \n-:()])*(?={end_word})", string=txt).group(0)
     found = found.replace("\\n", "")
-    found = found.replace("Program", "")
-    found = re.sub("^[\\d]{4}", "", found)
     found = found.strip()
+    print(found)
     return found
 
 
