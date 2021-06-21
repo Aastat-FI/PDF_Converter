4fb7efba25fe9a7ac9d5a0162d32acdd79393be1
Mikael Roto <mikael.roto@aastat.fi>
2021-06-21 14:19:48 +0300

    Changed settings from csv -> json

--- settings.py
+++ settings.py
@@ -5,20 +5,20 @@ def init():
     global PARAMETERS
     PARAMETERS = {}
 
-    #with open("settings.json", "r") as JSON:
-    #    PARAMETERS = json.load(JSON)
+    with open("settings.json", "r") as JSON:
+        PARAMETERS = json.load(JSON)
 
-    with open('settings.csv', 'r') as csvfile:
-        read = csv.DictReader(csvfile)
-        for row in read:
-            try:
-                value = int(row["Value"])
-            except:
-                try:
-                    value = float(row["Value"])
-                except:
-                    value = row["Value"]
-            PARAMETERS[row["Setting"]] = value
+    #with open('settings.csv', 'r') as csvfile:
+    #    read = csv.DictReader(csvfile)
+    #    for row in read:
+    #        try:
+    #            value = int(row["Value"])
+    #        except:
+    #            try:
+    #                value = float(row["Value"])
+    #            except:
+    #                value = row["Value"]
+    #        PARAMETERS[row["Setting"]] = value
 
 
 def get_parameters():
